c878d5d5f278cce37585b9265108daf11ee04fb6
FR4NCISCO1106 <francisquito1120@gmail.com>
2025-03-17 10:49:47 -0400

    feat:dfjashfah

---
+++ tema-3/ejercicio-3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio2</title>
+</head>
+<body>
+<?php
+  $suma = 0;
+  while ($suma < 100) {
+    $suma = $suma + 5;
+    echo $suma . " ";
+  }
+?>
+</body>
+</html>

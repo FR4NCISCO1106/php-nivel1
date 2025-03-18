f460bcc565c40b8bd3eac60999e61f9f5749d2b5
FR4NCISCO1106 <francisquito1120@gmail.com>
2025-03-18 10:16:39 -0400

    iashioahs

--- tema-5/conexion.php
+++ tema-5/conexion.php
@@ -1,10 +1,8 @@
 <?php
-$link = mysqli_connect("localhost", "root", "", "php1_francisco");
+  $link = mysqli_connect("localhost", "root", "", "php1_francisco");
 
-if (!$link) {
-  die('Error de Conexión (' . mysqli_connect_errno() . ') '
-    . mysqli_connect_error());
-}
-
-echo 'Éxito... ' . mysqli_get_host_info($link) . "\n";
+  if (!$link) {
+    die('Error de Conexión (' . mysqli_connect_errno() . ') '
+      . mysqli_connect_error());
+  }
 ?>
